534c29083563b3d00ce4ed92ee730dadaccf3ef7
Puzant <PBAKJEJIAN@hotmail.com>
2019-08-09 23:50:16 +0300

    add vueClipBoard2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import VueClipboard from 'vue-clipboard2'
+
+VueClipboard.config.autoSetContainer = true;
+Vue.use(VueClipboard);
+library.add(faUserSecret);
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
