929f34561978f51e2a924c99fe0dfacc9eea68ea
Puzant <PBAKJEJIAN@hotmail.com>
2019-08-10 15:46:54 +0300

    move quotes data to seperate file quotes.js

---
+++ src/assets/quotes.js
@@ -0,0 +1,17 @@
+export default {
+    "quotes" : [
+      {"qoute": "Good Artists Copy, Great Artists Steal.", "author": "Pablo Picasso"},
+      {"qoute": "Argue with idiots, and you become an idiot.", "author": "Paul Graham"},
+      {"qoute": "Be yourself; everyone else is already taken.", "author":"Oscar Wilde"},
+      {"qoute": "Simplicity is the ultimate sophistication.", "author": "Leonardo Da Vinci"},
+      {"qoute": "Learn from the mistakes of others. You can't live long enough to make them all yourself.", "author": "Eleanor Roosevelt"},
+      {"qoute": "The most important thing to remember is this: to be ready at any moment to give up what you are for what you might become", "author": "W.E.B. Du Bois"},
+      {"qoute": "I know in my heart that man is good. That what is right will always eventually triumph. And there's purpose and worth to each and every life.", "author": "Ronald Reagan"},
+    
+      {"qoute": "Don't cry because it's over, smile because it happened", "author": "Dr.Seuss"},
+      {"qoute": "So many books, so little time.", "author": "Frank Zappa"},
+      {"qoute": "You only live once, but if you do it right, once is enough.", "author": "Mae West"},
+      {"qoute": "Be the change that you wish to see in the world.", "author": "Mahatma Gandhi"},
+    
+    ]
+  }
