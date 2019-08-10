ce2d4affbd848e8663d7e6c068f297b04a156d6b
Puzant <PBAKJEJIAN@hotmail.com>
2019-08-10 11:12:42 +0300

    npm install sweet alert

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import VueSweetalert2 from 'vue-sweetalert2';
 
 import VueClipboard from 'vue-clipboard2'
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 
 VueClipboard.config.autoSetContainer = true;
 Vue.use(VueClipboard);
 library.add(faUserSecret);
+Vue.use(VueSweetalert2);
+
  
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
